4b24061bbedccc494c779ab580b518f2286b8f40
chaitanya <sparkscj234@gmail.com>
2022-10-05 15:34:48 +0530

    resolved issues

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import {createRoot} from "react-dom/client"
 import { ThemeProvider, CssBaseline } from "@material-ui/core"
 import theme from "./theme"
 import * as serviceWorker from "./serviceWorker"
@@ -22,7 +22,8 @@ String.prototype.capitalize = function(){
   return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"))
+root.render(
   <React.StrictMode>
     <Router>
       <ThemeProvider theme={theme}>
@@ -30,9 +31,9 @@ ReactDOM.render(
         <App />
       </ThemeProvider>
     </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 )
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA

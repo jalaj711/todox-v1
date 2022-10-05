cec6bd99290ca82d1cc9744abffbcd46bcd4da1f
Tyler Geerdts <tygee713@gmail.com>
2022-10-05 16:12:51 -0400

    Fixed slice payload logic

--- src/redux/actions.js
+++
@@ -1,8 +0,0 @@
-export const toggleSnackbarOpen = (snackbarProps) => ({
-  type: "TOGGLE_SNACKBAR_OPEN",
-  ...snackbarProps,
-})
-
-export const toggleSnackbarClose = () => ({
-  type: "TOGGLE_SNACKBAR_CLOSE",
-})
--- src/redux/snackbarSlice.js
+++ src/redux/snackbarSlice.js
@@ -1,23 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const initialState = {
+  open: false,
+  text: "Snackbar text",
+  showActionButton: false,
+  actionButtonText: "undo",
+  actionButtonClickCallback: null,
+}
+
 export const snackbarSlice = createSlice({
   name: "snackbar",
-  initialState: {
-    open: false,
-    text: "Snackbar text",
-    showActionButton: false,
-    actionButtonText: "undo",
-    actionButtonClickCallback: null,
-  },
+  initialState,
   reducers: {
-    openSnackbar: (state) => {
-      state = {
-        open: true,
-        text: action.message,
-        showActionButton: action.showActionButton,
-        actionButtonText: action.actionButtonText,
-        actionButtonClickCallback: action.actionButtonClickCallback,
-      }
+    openSnackbar: (state, action) => {
+      state.open = true
+      state.text = action.payload.text || initialState.text
+      state.showActionButton = action.payload.showActionButton || initialState.showActionButton
+      state.actionButtonText = action.payload.actionButtonText || initialState.actionButtonText
+      state.actionButtonClickCallback = action.payload.actionButtonClickCallback || initialState.actionButtonClickCallback
     },
     closeSnackbar: (state) => {
       state.open = false
@@ -25,6 +25,6 @@ export const snackbarSlice = createSlice({
   },
 })
 
-export const { open, close } = snackbarSlice.actions
+export const { openSnackbar, closeSnackbar } = snackbarSlice.actions
 
 export default snackbarSlice.reducer

1622cb4ab5e3e4a56af0fab4c199b64d2d20c0ae
jalaj711 <96870071+jalaj711@users.noreply.github.com>
2022-10-06 07:39:20 +0000

    Update deploy with master

---
+++ precache-manifest.6728eb4671288049e11dcc34679bc268.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b0fca212f40cc1b0f8f2c32db5bbbc1",
+    "url": "/todox-v1/index.html"
+  },
+  {
+    "revision": "aff7d7526d0ae83566e3",
+    "url": "/todox-v1/static/js/0.6a39691f.chunk.js"
+  },
+  {
+    "revision": "8fc4f443f0945d3d2aa8",
+    "url": "/todox-v1/static/js/1.0ab94eb5.chunk.js"
+  },
+  {
+    "revision": "957c69b7f81db988daac",
+    "url": "/todox-v1/static/js/10.82364aca.chunk.js"
+  },
+  {
+    "revision": "46fda3edbb153df262e5",
+    "url": "/todox-v1/static/js/11.9e7f835c.chunk.js"
+  },
+  {
+    "revision": "d60cde81a0f5bba64773",
+    "url": "/todox-v1/static/js/12.dd1bd282.chunk.js"
+  },
+  {
+    "revision": "5a9ae52c6de3bf4f5f7b",
+    "url": "/todox-v1/static/js/13.e76955e1.chunk.js"
+  },
+  {
+    "revision": "7b24d0dbc0e9428d2446",
+    "url": "/todox-v1/static/js/14.345d0ae4.chunk.js"
+  },
+  {
+    "revision": "8b485f7c84bf33bb4080",
+    "url": "/todox-v1/static/js/15.85784751.chunk.js"
+  },
+  {
+    "revision": "3ce44eb2eae6e381cf6c",
+    "url": "/todox-v1/static/js/16.f53e399b.chunk.js"
+  },
+  {
+    "revision": "b2791a28a6b8599bc079",
+    "url": "/todox-v1/static/js/17.ba08a64c.chunk.js"
+  },
+  {
+    "revision": "257fd3522d0982ab0568",
+    "url": "/todox-v1/static/js/2.a0e56a07.chunk.js"
+  },
+  {
+    "revision": "b6c725dce6794f06b791",
+    "url": "/todox-v1/static/js/3.73a7a7a0.chunk.js"
+  },
+  {
+    "revision": "102b6d40558cf7c07e5f",
+    "url": "/todox-v1/static/js/4.a7df7a22.chunk.js"
+  },
+  {
+    "revision": "2d5e01376064aa789a28",
+    "url": "/todox-v1/static/js/5.08c56f23.chunk.js"
+  },
+  {
+    "revision": "d24c92e255f3b9a8eccf",
+    "url": "/todox-v1/static/js/6.989b12ad.chunk.js"
+  },
+  {
+    "revision": "b8063371e02bdad8ab33",
+    "url": "/todox-v1/static/js/9.c3035375.chunk.js"
+  },
+  {
+    "revision": "d6363e525f186017144acd2ee6253cef",
+    "url": "/todox-v1/static/js/9.c3035375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de29d2340989317cdc0",
+    "url": "/todox-v1/static/js/main.7a00cfe5.chunk.js"
+  },
+  {
+    "revision": "fd747cf0ad8581658a59",
+    "url": "/todox-v1/static/js/runtime-main.ce00f761.js"
+  },
+  {
+    "url": "/todox/bg_offline.jpg",
+    "revision": "SmxquN8IDCqSnYTYCQoVyhgM"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todox/precache-manifest.5c1d7b32f1c5245694a581debfe83e85.js"
+  "/todox-v1/precache-manifest.6728eb4671288049e11dcc34679bc268.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todox/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todox-v1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
